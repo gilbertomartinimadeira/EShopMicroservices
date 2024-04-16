b3ef70a4b7aac9d4014607cbf81a40df121d5153
Gilberto Martini Madeira <gilberto.madeira@outlook.com>
2024-04-16 18:12:17 -0300

    Aded discount grpc api

---
+++ src/Services/Discount/Discount.Grpc/Models/Coupon.cs
@@ -0,0 +1,12 @@
+namespace Discount.Grpc.Models;
+
+public class Coupon
+{
+    public int Id { get; set; }
+    public sbyte ProductName { get; set; }
+    public sbyte ProductDescription { get; set; }
+    public int Amount {get;set;}
+
+    
+
+}
