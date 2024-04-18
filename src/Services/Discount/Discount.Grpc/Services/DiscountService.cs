045e1ebde5515bcc233d73b382d22a76433096d4
Gilberto Martini <gilberto.madeira@outlook.com>
2024-04-18 18:39:37 -0300

    Added the first connection to sqlite

--- src/Services/Discount/Discount.Grpc/Services/DiscountService.cs
+++ src/Services/Discount/Discount.Grpc/Services/DiscountService.cs
@@ -1,6 +1,7 @@
 using Discount.Grpc.Data;
 using Discount.Grpc.Models;
 using Grpc.Core;
+using Microsoft.EntityFrameworkCore;
 using static Discount.Grpc.DiscountProtoService;
 
 namespace Discount.Grpc.Services;
@@ -11,18 +12,23 @@ public class DiscountService(DiscountContext db, ILogger<DiscountService> logger
     {
         // TODO: GetDiscount from database
         logger.LogInformation("Fetching coupon from database...");
-        var coupon = await db.FindAsync<Coupon>(request.ProductName);
+        var coupon = await db.Coupons.FirstOrDefaultAsync(p => p.ProductName == request.ProductName);
 
-        if(coupon is null) {
-            logger.LogInformation("Coupon was not found");
+        if (coupon is null)
+        {
+
+            coupon = new Coupon { ProductName = "No Discount", Amount = 0, ProductDescription = "No Discount Desc" };
         };
 
-        return new CouponModel() {
-            Amount = coupon?.Amount ?? 0,
-            Description = coupon?.ProductDescription ?? "",
-            ProductName = coupon?.ProductName ?? request.ProductName
+        logger.LogInformation("Coupon was not found");
+        return new CouponModel()
+        {
+            Amount = coupon.Amount,
+            Description = coupon.ProductDescription,
+            ProductName = coupon.ProductName
         };
-    
+
+
     }
 
     public override Task<CouponModel> CreateDiscount(CreateDiscountRequest request, ServerCallContext context)
